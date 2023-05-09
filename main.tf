84b026f60ca0f7383e73b212bcdf772f305390e3
vagrant <vagrant@ubuntu1804.localdomain>
2023-05-09 04:53:16 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "serupadenganmu"
+    workspaces {
+      name = "aws"
+    }
+  }
+}
+
+//--------------------------------------------------------------------
+// Modules
+module "vpc_test" {
+  source  = "app.terraform.io/serupadenganmu/vpc-test/aws"
+  version = "0.1.0"
+}
